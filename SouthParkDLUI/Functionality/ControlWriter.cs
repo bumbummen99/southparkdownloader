3753613d1c3ec8dab50e23fa3a6c31ae32846b95
Patrick Henninger <privat@skyraptor.eu>
2021-02-21 12:02:47 +0100

    Remove Frontend and recreate as UI with NetCore3

---
+++ SouthParkDLUI/Functionality/ControlWriter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Windows.Controls;
+
+namespace SouthParkDLUI.Functionality
+{
+    public class ControlWriter : TextWriter
+    {
+        private TextBlock textbox;
+        public ControlWriter(TextBlock textbox)
+        {
+            this.textbox = textbox;
+        }
+
+        public override void Write(char value)
+        {
+            base.Write(value);
+            textbox.Dispatcher.BeginInvoke(new Action(() =>
+            {
+                textbox.Text += value.ToString();
+            }));
+        }
+
+        public override void Write(string value)
+        {
+            base.Write(value);
+            textbox.Dispatcher.BeginInvoke(new Action(() =>
+            {
+                textbox.Text += value + "\r\n";
+            }));
+        }
+
+        public override Encoding Encoding
+        {
+            get { return Encoding.ASCII; }
+        }
+    }
+}
